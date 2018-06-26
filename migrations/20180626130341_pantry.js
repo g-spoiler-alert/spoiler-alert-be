fb1a67e92a22420cef7c0a083eebff543daf75f3
yhtomitim <33264110+yhtomitim@users.noreply.github.com>
2018-06-26 13:15:22 -0600

    create migration for pantry and user tables

--- migrations/20180626130341_pantry.js
+++ migrations/20180626130341_pantry.js
@@ -1,8 +1,14 @@
 
-exports.up = function(knex, Promise) {
-  
+module.exports.up = (knex, Promise) => {
+  return knex.schema.createTable('item', (item) => {
+    item.increments().primary();
+    item.integer('user_id').references('user.id').unsigned().onDelete('CASCADE');
+    item.text('name');
+    item.text('type');
+    item.date('expDate');
+  })
 };
 
-exports.down = function(knex, Promise) {
-  
+module.exports.down = (knex, Promise) => {
+  return knex.scheme.dropTableIfExists('item');
 };
