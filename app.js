28d4191e62ae24fdc45f0af99c5903ceb0fd9db0
garth stevens <gstevens96@gmail.com>
2018-06-26 13:49:40 -0600

    created the routes

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
+const cors = require("cors");
+const route = require("./router/routes")
 const app = express();
 
 app.use(bodyParser.json());
 app.use(morgan('dev'));
+app.use('/api/v1/', route)
 
-app.get('/', (req, res, next) => {
-  res.send({message: 'this is working, but under construction!'})
-})
 module.exports = app;
