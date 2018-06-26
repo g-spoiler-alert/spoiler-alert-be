ea738e948a90acdb8c394aa7b8767e3736f7c01a
garth stevens <gstevens96@gmail.com>
2018-06-26 14:00:38 -0600

    finished the routes

--- router/routes.js
+++ router/routes.js
@@ -1,3 +1,14 @@
-app.get('/', (req, res, next) => {
+const express = require("express");
+const router = express.Router()
+const queries = require('../queries')
+
+router.get('/', (req, res, next) => {
   res.send({message: 'this is working, but under construction!'})
 })
+
+router.get('/pantry', (req, res, next) => {
+  queries.readItem() 
+    .then(items => {res.json({items})})
+})
+
+module.exports = router
