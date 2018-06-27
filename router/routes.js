0b0f4eeb74b59fe54c8f725cbaf7ca1c16ddfa4e
yhtomitim <33264110+yhtomitim@users.noreply.github.com>
2018-06-27 06:39:53 -0600

    refactor for error handling and naming convention

--- router/routes.js
+++ router/routes.js
@@ -3,12 +3,14 @@ const router = express.Router()
 const queries = require('../queries')
 
 router.get('/', (req, res, next) => {
-  res.send({message: 'this is working, but under construction!'})
+  res.send({ message: 'this is working, but under construction!' })
+  .catch(next)
 })
 
 router.get('/pantry', (req, res, next) => {
-  queries.readItem() 
-    .then(items => {res.json({items})})
+  queries.readItem()
+    .then(items => { res.json({ items }) })
+    .catch(next);
 })
 
 router.get('/users', (req, res, next) => {
@@ -18,6 +20,7 @@ router.get('/users', (req, res, next) => {
         users
       })
     })
+    .catch(next);
 })
 
 module.exports = router
