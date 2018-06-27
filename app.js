9884979fa91a671823a1d7db6a98003d36d0afd5
yhtomitim <33264110+yhtomitim@users.noreply.github.com>
2018-06-27 06:39:53 -0600

    refactor for error handling and naming convention

--- app.js
+++ app.js
@@ -2,11 +2,27 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const cors = require("cors");
-const route = require("./router/routes")
+const routes = require("./router/routes")
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 app.use(morgan('dev'));
-app.use('/api/v1/', route)
+app.use('/api/v1/', routes)
+
+app.use((req, res, next) => {
+  const err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: req.app.get("env") === "development" ? err.stack : {}
+  });
+});
+
 
 module.exports = app;
